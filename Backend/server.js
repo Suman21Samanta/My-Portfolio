6cf2c945461eb7a980024bdb51b415d5a641c5cd
Suman Samanta <samantasuman85004@gmail.com>
2026-08-16 11:27:37 +0530

    Prepare server for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -307,11 +307,10 @@ app.use((req, res, next) => {
 
 app.listen(
     PORT,
+    "0.0.0.0",
     () => {
-
         console.log(
-            `Portfolio running at http://localhost:${PORT}`
+            `Portfolio running on port ${PORT}`
         );
-
     }
 );
